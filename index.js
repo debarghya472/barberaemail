994c13e0652f8f426ba34a8804c170eb57d3fa68
debarghya472 <debarghya_1901me19@iitp.ac.in>
2021-03-10 14:03:34 +0530

    cancel booking

--- index.js
+++ index.js
@@ -25,10 +25,56 @@ app.post('/send',(req,res)=>{
 app.get('/test',(req,res)=>{
     res.send("HEllo Test")
 });
+app.post('/cancel',(req,res)=>{
+    console.log(req.body.email);
+    if(cancelsend(req,res)){
+       res.json({"result":"success"});
+    }
+});
 
 app.listen(port, () => {
     console.log('Server started at port 2000....');
 });
+
+async function cancelsend(req,res){
+    const emailbody = `
+    <p><b>Your Booking has been Cancelled</b></p>
+
+    <p>Your Booking for the following services has been Cancelled</p>`+req.body.summary+`
+    
+    <p>Regards<br>
+    Team Barbera</p>
+  `;
+    let transporter = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 587,
+        secure: false, // true for 465, false for other ports
+        auth: {
+            user: config.email, // generated ethereal user
+            pass: config.password  // generated ethereal password
+        },
+        tls: {
+            rejectUnauthorized: false
+        }
+    });
+    let mailOptions = {
+        from: '"Barbera" <debarghyamaity@gmail.com>', // sender address
+        to: req.body.email, // list of receivers
+        subject: "Booking Cancellation", // Subject line // plain text body
+        html: emailbody// html body
+    };
+    var success =false;
+    await transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.log(error);
+            return false;
+        }
+        console.log('Message sent: %s', info.messageId);
+        success = true;
+        return success;
+        // console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+    });
+}
 
 async function sendmail(req,res){
     const emailbody = `
